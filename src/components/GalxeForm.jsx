8d2947daf5ebe2aff3d63c03eef884d1d2adecad
kumardevofficial <112534262+kumardevofficial@users.noreply.github.com>
2024-12-29 20:38:59 +0530

    adding ver json

--- src/components/GalxeForm.jsx
+++ src/components/GalxeForm.jsx
@@ -7,7 +7,7 @@ const GalxeForm = () => {
   };
 
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+    <div className="flex  justify-center items-center min-h-screen bg-gray-100">
       <div className="bg-white shadow-lg rounded-lg p-8 w-full max-w-md">
         <h2 className="text-2xl font-bold text-gray-800 text-center mb-6">
           Galxe Form
